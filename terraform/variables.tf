27bd9f172b7069ec30ee339a348251d6bf95a896
Meenu <22242356+meenuc95@users.noreply.github.com>
2025-06-25 20:52:07 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,8 +3,10 @@ variable "vpc_cidr"          { default = "10.0.0.0/16" }
 variable "public_cidr"       { default = "10.0.1.0/24" }
 variable "private_cidr"      { default = "10.0.2.0/24" }
 variable "az"                { default = "us-east-1a" }
-variable "ssh_key_name"      { default = "jenkins-key" }
-variable "ssh_pubkey_path"   { default = "keys/jenkins-key.pub" }
-variable "private_key_path"  { default = "/home/ubuntu/.ssh/jenkins-key" }
+
+# Use an existing keypair name that already exists in AWS
+variable "ssh_key_name"      { default = "ubuntu-slave-jen" }
+variable "private_key_path"  { default = "/home/ubuntu/.ssh/ubuntu-slave-jen.pem" }
+
 variable "bastion_ami"       { default = "ami-020cba7c55df1f615" }
 variable "mongo_ami"         { default = "ami-020cba7c55df1f615" }
